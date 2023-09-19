3913aeced32aad2ead31ab4130b5b7fc0a5456b3
dsgnrr <semeniuk.oleg2002@gmail.com>
2023-09-19 18:59:34 +0300

    Д.З. Реалізувати відображення вмісту поточної папки у формі команди "ls"/"dir" Додати скриншоти до репозиторію
    Mode                 LastWriteTime         Length Name
    ----                 -------------         ------ ----
    d-----        19.09.2023     16:09                .idea
    d-----        18.09.2023     12:24                src
    d-----        18.09.2023     12:54                target
    -a----        18.09.2023     12:23            490 .gitignore
    -a----        19.09.2023     15:09           1093 pom.xml

--- src/main/java/step/learning/basics/FilesDemo.java
+++ src/main/java/step/learning/basics/FilesDemo.java
@@ -1,6 +1,11 @@
 package step.learning.basics;
 
+import jdk.nashorn.internal.parser.DateParser;
+
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class FilesDemo {
     public void run() {
@@ -18,12 +23,38 @@ public class FilesDemo {
                 dir.isDirectory() ? "directory" : "file");
 
         System.out.println(dir.getAbsolutePath());
-        for (String filename : dir.list()) {    // dir.list() - лише імена (String)
-            System.out.println(filename);       // dir.listFiles() - об'єкти (Files)
+//        for (String filename : dir.list()) {    // dir.list() - лише імена (String)
+//            System.out.println(filename);       // dir.listFiles() - об'єкти (Files)
+//        }
+        System.out.println(String.format("%-6s %-17s %-8s %s", "Mode", "LastWriteTime", "Length", "Name"));
+        System.out.println(String.format("%-6s %-17s %-8s %s","----","-------------","------","----"));
+        for (File file : dir.listFiles()) {
+            PrintFileInfo(file);
         }
 
     }
+    private void PrintFileInfo(File file){
+        System.out.println(
+                String.format("%-6s %-17s %-8s %s",
+                        getMode(file),
+                        getDate(file.lastModified()),
+                        getLength(file),
+                        file.getName()));
+    }
+    private String getMode(File file){
+        return file.isDirectory()?"d-----":"-a----";
+    }
+    private String getLength(File file){
+        return file.isFile()?String.format("%s",file.length()):"";
+    }
+    private String getDate(long milliseconds) {
+        Date date = new Date(milliseconds);
+        Locale.setDefault(Locale.US);
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        return dateFormat.format(date);
+    }
 }
+
 /* Робота з файлами розглядається у двох аспектах:
     - робота з файловую системою: створення файлів, пошук, переміщення, видалення, тощо
     - використання файлів для збереження/віднолвення даних
