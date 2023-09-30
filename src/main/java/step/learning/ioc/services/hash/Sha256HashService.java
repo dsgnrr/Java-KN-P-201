0a3baa3561f93d200636e10c40519930cc52b246
dsgnrr <semeniuk.oleg2002@gmail.com>
2023-09-30 16:26:19 +0300

    Homework 8. Д.З. Налаштувати проєкт для роботи з інверсією управління з інжектором Guice Реалізувати другу імплементацію сервісу гешування (SHA, SHA-2, інші - на вибір) Замінити прив'язку сервісів, переконатись у їх роботі. Додавати скріншоти.

---
+++ src/main/java/step/learning/ioc/services/hash/Sha256HashService.java
@@ -0,0 +1,21 @@
+package step.learning.ioc.services.hash;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256HashService implements HashService {
+    @Override
+    public String hash(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest.digest(input.getBytes(StandardCharsets.UTF_8))) {
+                sb.append(String.format("%02x", b & 0XFF));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
